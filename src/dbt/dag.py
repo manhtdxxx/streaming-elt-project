8522dd3e1214f30947a3cc1aeb70ee0d2a505c9f
Tran Duc Manh <tdmanh1510@gmail.com>
2025-08-03 14:32:06 +0700

    .

--- src/dbt/dag.py
+++ src/dbt/dag.py
@@ -3,35 +3,43 @@ from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 
+# define path in container
+profiles_dir = "/home/airflow/.dbt"
+project_dir = "/opt/airflow/dags/nyc_taxi"
+target_env = "prod"
+
+# common config for dag
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'owner': 'manh',
+    'depends_on_past': False,  # set True if previous day failed, then skip today
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1),
 }
 
 with DAG(
     dag_id='dbt_run_and_test',
     default_args=default_args,
-    description='Run dbt models and tests',
-    schedule_interval='@daily',
     start_date=days_ago(1),
-    catchup=False,
+    schedule_interval=timedelta(minutes=1),
+    catchup=False,  # set True to run from start date to now
+    description='Run dbt models and tests',
     tags=['dbt'],
 ) as dag:
 
     # dbt run
     dbt_run = BashOperator(
         task_id='dbt_run',
-        bash_command='dbt run --profiles-dir /home/airflow/.dbt --project-dir /opt/airflow/dags',
+        bash_command=(
+            f"dbt run --profiles-dir {profiles_dir} --project-dir {project_dir} --target {target_env}"
+        ),
     )
 
     # dbt test
     dbt_test = BashOperator(
         task_id='dbt_test',
-        bash_command='dbt test --profiles-dir /home/airflow/.dbt --project-dir /opt/airflow/dags',
+        bash_command=(
+            f"dbt test --profiles-dir {profiles_dir} --project-dir {project_dir} --target {target_env}"
+        ),
     )
 
     dbt_run >> dbt_test
